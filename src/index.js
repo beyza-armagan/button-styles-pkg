889816cbbdc483cf6076c04bf6f9320d4ce6f186
Beyza Armağan <70449529+beyza-armagan@users.noreply.github.com>
2023-12-21 16:43:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faDownload,
+  faSpinner,
+  faHandPointer
+} from '@fortawesome/free-solid-svg-icons'
 
 export const Button = ({ text, buttonType }) => {
   const classes = `${styles.buttonItem} ${styles[buttonType]}`
   return (
     <div>
-      <button className={classes}>{text}</button>
+      <button className={classes}>
+        {buttonType === 'loading' ? (
+          <span>
+            <FontAwesomeIcon icon={faSpinner} spin /> {text}
+          </span>
+        ) : buttonType === 'download' ? (
+          <span>
+            <FontAwesomeIcon icon={faDownload} /> {text}
+          </span>
+        ) : buttonType === 'click' ? (
+          <span>
+            <FontAwesomeIcon icon={faHandPointer} /> {text}
+          </span>
+        ) : (
+          text
+        )}
+      </button>
     </div>
   )
 }
